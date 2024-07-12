0530254cabd8d0b97a7f82f6b51253f592faa2d5
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2024-07-13 01:14:47 +0530

    updated

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,35 +1,47 @@
 const Header = () => {
   return (
-    <div className="bg-lime-50 bg-transparent w-screen text-lime-700 h-20  justify-between flex flex-row items-center px-4">
-      <div className="text-2xl font-extrabold font-WtfHorseland  p-2 pt-3">
+    <div className="bg-lime-50 bg-transparent w-full text-yellow-300 h-20  justify-between flex flex-row items-center px-4 absolute top-0">
+      <div className=" lg:text-4xl text-2xl font-extrabold font-WtfHorseland  p-2 pt-3">
         ShopTracker
       </div>
 
       <div className="btn-group gap-2 flex align-middle max-md:hidden  justify-around">
         <a
           href=""
-          className="btn bg-lime-950 px-2 py-1 rounded hover:bg-white hover:text-lime-950 text-lime-200 duration-150 shadow-black shadow-2xl"
+          className="relative border border-yellow-300 py-1 px-4 rounded bg-transparent text-yellow-300 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-yellow-300 before:transition-all before:duration-500 hover:text-lime-900 hover:shadow-red-500 hover:before:w-full"
         >
+          <span
+          className="z-10 relative">
           Home
+          </span>
         </a>
         
         <a
           href=""
-          className="btn bg-lime-950 px-2 py-1 rounded hover:bg-white hover:text-lime-950 text-lime-200 duration-150 shadow-black shadow-2xl"
-        >
+          className="relative border border-yellow-300 py-1 px-4 rounded bg-transparent text-yellow-300 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-yellow-300 before:transition-all before:duration-500 hover:text-lime-900 hover:shadow-red-500 hover:before:w-full"
+          >
+            <span
+            className="z-10 relative">
           Catalog
+          </span>
         </a>
         <a
           href=""
-          className="btn bg-lime-950 px-2 py-1 rounded hover:bg-white hover:text-lime-950 text-lime-200 duration-150 shadow-black shadow-2xl"
+          className="relative border border-yellow-300 py-1 px-4 rounded bg-transparent text-yellow-300 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-yellow-300 before:transition-all before:duration-500 hover:text-lime-900 hover:shadow-red-500 hover:before:w-full"
         >
+          <span
+          className="z-10 relative">
           Shop
+            </span>
         </a>
         <a
           href=""
-          className="btn bg-lime-950 px-2 py-1 rounded hover:bg-white hover:text-lime-950 text-lime-200 duration-150 shadow-black shadow-2xl"
-        >
+          className="relative border border-yellow-300 py-1 px-4 rounded bg-transparent text-yellow-300 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-yellow-300 before:transition-all before:duration-500 hover:text-lime-900 hover:shadow-red-500 hover:before:w-full"
+          >
+            <span
+            className="z-10 relative">
           Contact
+          </span>
         </a>
       </div>
       
@@ -41,7 +53,7 @@ const Header = () => {
         name=""
         id=""
         placeholder="Search.."
-        className=" px-2 py-1 rounded border-lime-950 border"
+        className=" px-2 py-1 rounded border-yellow-300 border bg-lime-950 text-yellow-300 placeholder-lime-100 "
       />
       <button className="btn bg-lime-950 px-3 py-2 rounded hover:bg-white hover:text-lime-950 text-lime-200 duration-150 shadow-black shadow-2xl">
         Login
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,17 +1,33 @@
 import Header from "../common/Header";
-import Footer from "../common/Footer";
 
 const HomePage = () => {
   return (
-    <div className="bg-hero-pattern  bg-center  bg-cover w-full h-screen">
-      <Header />
-      <div>
-        <h1 className="text-5xl w-full h-[80vh] bg-gradient-to-tr from-lime-900 to-white-100 bg-opacity-10 font-bold text-white text-center">Welcome to the
-          <span className="text-red-500"> Home Page</span></h1>
-          
+    <section>
+      <div className=" from-lime-900 to-white text-white py-20 bg-[url('https://images.unsplash.com/photo-1506617564039-2f3b650b7010?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-blend-darken bg-cover bg-center bg-no-repeat bg-lime-800">
+        <Header />
+        <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
+          <div className="flex flex-col w-full  justify-center  items-center p-8">
+            <h1 className="text-5xl md:text-9xl p-2 text-yellow-300 tracking-loose font-WtfHorseland">
+              <span className="text-white ">hey <br /></span>Make healthy life with fresh grocery
+            </h1>
+            {/* <h2 className="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">
+              Space : The Timeless Infinity
+            </h2> */}
+            <p className="text-sm  text-center text-gray-50 mb-4">
+              Explore your favourite events and register now to showcase your
+              talent and win exciting prizes.
+            </p>
+
+            <a
+              href="#"
+              className="relative border border-yellow-300 py-2 px-4 rounded bg-transparent text-yellow-300 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-0 before:w-full before:bg-yellow-300 before:transition-all before:duration-500 hover:text-lime-900 hover:shadow-red-500 hover:before:h-full"
+            >
+              <span className="relative z-10">Explore Now</span>
+            </a>
+          </div>
+        </div>
       </div>
-      <Footer />
-    </div>
+    </section>
   );
 };
 
