0d772df9543875a1e9980fbf923ed510a3711787
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2024-07-30 13:18:15 +0530

    updated

--- src/components/common/Category.tsx
+++ src/components/common/Category.tsx
@@ -1,11 +1,93 @@
-import React from 'react'
+import React from "react";
 
 const Category = () => {
   return (
     <section className="catagory w-full h-screen">
-        hey
-    </section>
-  )
-}
+      <div className="catagory flex flex-row align-middle items-center gap-2 overflow-x-scroll ">
+        <div className=" bg-yellow-200 flex flex-col align-middle items-center ">
+          <img
+            src="https://images.unsplash.com/photo-1516594798947-e65505dbb29d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className=" w-48 h-48 object-cover object-center p-3"
+          />
 
-export default Category
+          <h1 className="text-2xl font-bold">Category 1</h1>
+        </div>
+        <div className=" bg-yellow-200 flex flex-col align-middle items-center ">
+          <img
+            src="https://images.unsplash.com/photo-1516594798947-e65505dbb29d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className=" w-48 h-48 object-cover object-center p-3"
+          />
+
+          <h1 className="text-2xl font-bold">Category 1</h1>
+        </div>
+        <div className=" bg-yellow-200 flex flex-col align-middle items-center ">
+          <img
+            src="https://images.unsplash.com/photo-1516594798947-e65505dbb29d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className=" w-48 h-48 object-cover object-center p-3"
+          />
+
+          <h1 className="text-2xl font-bold">Category 1</h1>
+        </div>
+        <div className=" bg-yellow-200 flex flex-col align-middle items-center ">
+          <img
+            src="https://images.unsplash.com/photo-1516594798947-e65505dbb29d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className=" w-48 h-48 object-cover object-center p-3"
+          />
+
+          <h1 className="text-2xl font-bold">Category 1</h1>
+        </div>
+        <div className=" bg-yellow-200 flex flex-col align-middle items-center ">
+          <img
+            src="https://images.unsplash.com/photo-1516594798947-e65505dbb29d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className=" w-48 h-48 object-cover object-center p-3"
+          />
+
+          <h1 className="text-2xl font-bold">Category 1</h1>
+        </div>
+        <div className=" bg-yellow-200 flex flex-col align-middle items-center ">
+          <img
+            src="https://images.unsplash.com/photo-1516594798947-e65505dbb29d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className=" w-48 h-48 object-cover object-center p-3"
+          />
+
+          <h1 className="text-2xl font-bold">Category 1</h1>
+        </div>
+        <div className=" bg-yellow-200 flex flex-col align-middle items-center ">
+          <img
+            src="https://images.unsplash.com/photo-1516594798947-e65505dbb29d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className=" w-48 h-48 object-cover object-center p-3"
+          />
+
+          <h1 className="text-2xl font-bold">Category 1</h1>
+        </div>
+        <div className=" bg-yellow-200 flex flex-col align-middle items-center ">
+          <img
+            src="https://images.unsplash.com/photo-1516594798947-e65505dbb29d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className=" w-48 h-48 object-cover object-center p-3"
+          />
+
+          <h1 className="text-2xl font-bold">Category 1</h1>
+        </div>
+        <div className=" bg-yellow-200 flex flex-col align-middle items-center ">
+          <img
+            src="https://images.unsplash.com/photo-1516594798947-e65505dbb29d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className=" w-48 h-48 object-cover object-center p-3"
+          />
+
+          <h1 className="text-2xl font-bold">Category 1</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Category;
