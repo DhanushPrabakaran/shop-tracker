f82f2312cb7ce795b3940297365d295dd3ec5b44
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2024-07-30 21:45:23 +0530

    updared

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,76 @@ import { MdArrowOutward } from "react-icons/md";
 const Home = () => {
   return (
     <div className="text-black w-full min-h-screen flex lg:flex-row flex-col font-MintGrotesk">
-      <div className="h-screen flex flex-col items-start justify-center lg:w-1/2 ">
+
+      <div className="h-screen flex flex-col items-center justify-center lg:w-1/2 relative">
+      
+<span
+        className="absolute top-0 right-0  bg-primary w-full h-full  grid grid-cols-8 grid-rows-8  ">
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+          <div className="border border-transparent hover:border-primary-black ease-linear duration-300 hover:bg-slate-100 hover:shadow"></div>
+        </span>
+      <div
+      className="z-10">
+      
+        
         <h1 className=" res-heading-xl ">
           Hey <br />
           Make healthy life with fresh grocery
@@ -18,13 +87,14 @@ const Home = () => {
           <span className=" absolute w-full   bg-primary-black h-0 top-1/2 left-0 -z-40 group-hover:h-full group-hover:top-0 duration-500 ease-in-out"></span>
         </a>
       </div>
+      </div>
 
       <div className="lg:w-1/2 h-screen  flex flex-row">
         <div className="relative h-screen flex-grow w-[25vw]">
           <Image
             src={img1}
             alt={""}
-            className=" w-full bg-cover h-full  object-cover rounded-xl p-2 hover:object-right-top duration-1000"
+            className=" w-full bg-cover h-full  object-cover rounded-xl py-2 hover:object-right-top duration-1000"
           />
           <MdArrowOutward
             className="absolute right-0 top-0 fill-transparent "
